243ca64aedadfb889c4888504c49dbfa9057879d
Duda Becker <54290661+dudabecker@users.noreply.github.com>
2022-03-31 20:48:29 -0300

    Add files via upload

--- pesquisa.php
+++ pesquisa.php
@@ -1,29 +1,88 @@
 <?php
 include_once("conexao.php");
 $pesquisa = $_GET["pesquisa"];
+?>
 
-        $registrosNome = mysqli_query($conexao,"select nomeProduto, descricao, estoque, preco, categoria from
-        produto where nomeProduto like '%$pesquisa%' or categoria like '%$pesquisa%'");
+<!doctype html>
+    <html>
+    <head>
+    <title>pesquisa em nuesotro</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
 
-        $num_linhas = mysqli_num_rows($registrosNome);
+<body>
+    <a class="cabecalho-logo">nues<br>otro</a>
+    <header class="cabecalho">
+        <h1 class="logo">
+    
+        </h1>
+    </header>
+    
+    <form name="pesquisa" action="pesquisa.php" method="get">
+        <input type="text" name="pesquisa" id="pesquisa" placeholder="o que você procura?">
+        <input type="submit" value="Pesquisar">
+    </form>
 
-        for($i;$i<$num_linhas;$i++){
-        $dados = mysqli_fetch_array($registrosNome);
-        $nomeProduto = $dados["nomeProduto"];
-        $descricaoProduto = $dados["descricaoProduto"];
-        $estoque = $dados["estoque"];
-        $preco = $dados["preco"];
-        $categoria = $dados["categoria"];
-        //$imagem = $dados["imagem"];
 
-        echo "<div>
-                <div> imagem </div>
-                <div> $nomeProduto<br>
-                    $descricaoProduto<br><br>
-                    estoque: $estoque<br>
-                    preço unitário: $preco<br>
-                    categoria: $categoria<br><br>
-                </div>
-            </div>";
-        }
-    ?>
+            <?php
+
+            if(!(empty($pesquisa))){
+            $registrosNome= mysqli_query($conexao,"select nomeProduto, descricaoProduto, estoque, preco, categoria from
+            produto where nomeProduto like '%$pesquisa%' or categoria like '%$pesquisa%'");
+
+            $num_linhas = mysqli_num_rows($registrosNome);
+
+            if($num_linhas == 0) {
+                echo "nenhum produto encontrado!";
+            }
+            
+            else {
+
+            for($i=0;$i<$num_linhas;$i++){
+            $dados = mysqli_fetch_array($registrosNome);
+            $nomeProduto = $dados["nomeProduto"];
+            $descricaoProduto = $dados["descricaoProduto"];
+            $estoque = $dados["estoque"];
+            $preco = $dados["preco"];
+            $categoria = $dados["categoria"];
+            //$imagem = $dados["imagem"];
+
+            echo "<div>
+                    <div> imagem </div>
+                    <div> $nomeProduto<br>
+                          $descricaoProduto<br><br>
+                          estoque: $estoque<br>
+                          preço unitário: $preco<br>
+                          categoria: $categoria<br><br>
+                    </div>
+                  </div>";
+                }
+            }
+            }else{
+            $registrosNome = mysqli_query($conexao,"select * from produto");
+            $num_linhas = mysqli_num_rows($registrosNome);
+            for($i=0;$i<$num_linhas;$i++){
+               $dados = mysqli_fetch_array($registrosNome);
+               $nomeProduto = $dados["nomeProduto"];
+               $descricaoProduto = $dados["descricaoProduto"];
+               $estoque = $dados["estoque"];
+               $preco = $dados["preco"];
+               $categoria = $dados["categoria"];
+               //$imagem = $dados["imagem"];
+
+               echo "<div>
+                       <div> imagem </div>
+                       <div> $nomeProduto<br>
+                             $descricaoProduto<br><br>
+                             estoque: $estoque<br>
+                             preço unitário: $preco<br>
+                             categoria: $categoria<br><br>
+                       </div>
+                     </div>";
+                }
+            }
+            ?>
+        </body>
+    </html> 
