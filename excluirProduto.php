1edd9c1b24a3c57ae07290d4e54152f93a33c501
Duda Becker <54290661+dudabecker@users.noreply.github.com>
2022-04-04 22:03:42 -0300

    Delete excluirProduto.php

--- excluirProduto.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("conexao.php");
-$idProduto = $_POST["idProduto"];
-$sqlDeleta = mysqli_query($conexao,"delete from produto where idProduto = '$idProduto'") 
-or die("Erro ao deletar arquivo. " . mysqli_error($conexao));
-header('Location: pesquisa.php');
-?>
